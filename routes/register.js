3c78ec650196b0d04de503af7b01306a79a473b2
soumyaranjan <51379230+coderstab@users.noreply.github.com>
2023-09-26 19:38:56 +0530

    add data to database after register success

--- routes/register.js
+++ routes/register.js
@@ -4,9 +4,9 @@ const supabase = require('../db/db'); // Adjust the path as needed
 const authorize = require('../middleware/authorize');
 
 // Define the registration route
-router.post('/register',authorize, async (req, res) => {
+router.post('/register', authorize, async (req, res) => {
   try {
-    const { email, password } = req.body;
+    const { email, password,name,phone_number } = req.body;
 
     // Check if the email already exists in the database
     const { data, error } = await supabase
@@ -36,7 +36,26 @@ router.post('/register',authorize, async (req, res) => {
     }
 
     // User registered successfully
+
+    // Insert user data into the 'user' table
+    const userDataToInsert = {
+      email,
+      name,
+      phone_number // Assuming you want to insert the email into the 'email' column
+      // Add other user data fields here if needed
+    };
+
+    const { userInsertData, userInsertError } = await supabase
+      .from('users')
+      .upsert([userDataToInsert], { onConflict: ['email'] });
+
+    if (userInsertError) {
+      console.error('User data insert error:', userInsertError);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
     return res.status(201).json({ message: 'Registration successful' });
+
   } catch (error) {
     console.error('Server error:', error);
     return res.status(500).json({ error: 'Internal Server Error' });
