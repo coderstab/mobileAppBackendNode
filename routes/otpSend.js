75ca178c3b767e243f4cce9a39147c686f7f28f7
soumyaranjan <51379230+coderstab@users.noreply.github.com>
2023-09-26 14:45:46 +0530

    add axios to avoid problem

--- routes/otpSend.js
+++ routes/otpSend.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const supabase = require('../db/db'); // Adjust the path as needed
 const authorize = require('../middleware/authorize');
-const unirest = require('unirest');
+const axios = require('axios'); // Import axios
 require('dotenv').config();
-
 
 router.post('/send-otp', async (req, res) => {
   const { phoneNumber } = req.body;
@@ -13,19 +12,18 @@ router.post('/send-otp', async (req, res) => {
   // Generate a random 6-digit OTP
   const otp = String(Math.floor(1000 + Math.random() * 9000));
 
-
   // Store the OTP in Supabase
- const { data, error } = await supabase
-  .from('otp')
-  .upsert(
-    [
-      {
-        phone_number: phoneNumber,
-        otp: otp,
-      },
-    ],
-    { onConflict: ['phone_number'], updateAll: ['otp'] }
-  );
+  const { data, error } = await supabase
+    .from('otp')
+    .upsert(
+      [
+        {
+          phone_number: phoneNumber,
+          otp: otp,
+        },
+      ],
+      { onConflict: ['phone_number'], updateAll: ['otp'] }
+    );
 
   if (error) {
     return res.status(500).json({ error: 'Failed to send OTP' });
@@ -34,30 +32,32 @@ router.post('/send-otp', async (req, res) => {
   // Send the OTP via Fast2SMS
   const fast2smsEndpoint = 'https://www.fast2sms.com/dev/bulkV2';
 
-  const request = unirest.post(fast2smsEndpoint);
+  try {
+    const response = await axios.post(
+      fast2smsEndpoint,
+      {
+        variables_values: otp,
+        route: 'otp',
+        numbers: phoneNumber,
+      },
+      {
+        headers: {
+          Authorization: smsApi, // Replace with your Fast2SMS API key
+        },
+      }
+    );
 
-  // Set Fast2SMS headers and form data
-  request.headers({
-    'authorization': `${smsApi}`, // Replace with your Fast2SMS API key
-  });
-
-  request.form({
-    'variables_values': `${otp}`,
-    'route': 'otp',
-    'numbers': phoneNumber,
-  });
-
-  // Send the request to Fast2SMS
-  request.end(function (response) {
-    if (response.error) {
-      console.error(response.error);
-      return res.status(500).json({ error: 'Failed to send OTP ' });
+    // Check if Fast2SMS request was successful
+    if (response.status === 200) {
+      // For testing purposes, we'll just return the OTP in the response
+      return res.json({ message: 'OTP has been sent successfully.' });
+    } else {
+      return res.status(500).json({ error: 'Failed to send OTP' });
     }
-
-    // For testing purposes, we'll just return the OTP in the response
-    res.json({ message: 'OTP has sent successfully.' });
-  });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Failed to send OTP' });
+  }
 });
-
 
 module.exports = router;
